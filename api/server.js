ea9d0462b954ed6a989d5fb6ec4ef3abc058ce40
Fatimah Sarwar <fatimahsarwar@Fatimahs-MBP.fios-router.home>
2022-03-09 20:02:17 -0500

    finished

--- api/server.js
+++ api/server.js
@@ -1,10 +1,24 @@
 const express = require('express');
-
+const userRouter = require('./users/users-router')
 const server = express();
 
 // remember express by default cannot parse JSON in request bodies
-
+server.use(express.json());
+server.use(logger);
 // global middlewares and the user's router need to be connected here
+server.use('/api/users', userRouter)
+
+
+function logger(req, res, next) {
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get(
+      'Origin'
+    )}`
+  );
+
+  next();
+}
+
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
